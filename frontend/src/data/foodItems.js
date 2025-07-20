854da022ee76d1eca5f016a848e6b8a8ed8ba1d5
Mohammed Shabaaz <mohammedshabaaz0199@gmail.com>
2025-07-20 12:32:09 +0530

    Add food items

--- frontend/src/data/foodItems.js
+++ frontend/src/data/foodItems.js
@@ -1,59 +1,76 @@
+import Burger from "../assets/burger.png";
+import Pizza from "../assets/pizza.png";
+import Fries from "../assets/french-fries.png";
+import Pancake from "../assets/pancake.png";
+import Sizzling from "../assets/sizzling-brownie.png";
+import Shortcake from "../assets/strawberry-shortcake.png";
+import Iced_Mocha from "../assets/iced-mocha.png";
+import Mocktail from "../assets/mocktail.png";
+import Smoothie from "../assets/smoothie.png";
+
 const foodItems = [
     {
       id: 1,
       category: "Fast Food",
       item: "Burger",
       price: 120,
-      image: "https://placehold.co/220x135",
+      image: Burger,
     },
     {
       id: 2,
       category: "Fast Food",
       item: "Pizza",
       price: 110,
-      image: "https://placehold.co/220x135",
+      image: Pizza,
     },
     {
       id: 3,
       category: "Fast Food",
       item: "Fries",
       price: 100,
-      image: "https://placehold.co/220x135",
+      image: Fries,
     },
     {
       id: 4,
       category: "Desserts",
-      item: "Ice Cream",
+      item: "Pancake",
       price: 80,
-      image: "https://placehold.co/220x135",
+      image: Pancake,
     },
     {
       id: 5,
       category: "Desserts",
-      item: "Cupcake",
+      item: "Sizzling",
       price: 90,
-      image: "https://placehold.co/220x135",
+      image: Sizzling,
     },
     {
       id: 6,
-      category: "Drinks",
-      item: "Cola",
-      price: 50,
-      image: "https://placehold.co/220x135",
+      category: "Desserts",
+      item: "Shortcake",
+      price: 90,
+      image: Shortcake,
     },
     {
       id: 7,
       category: "Drinks",
-      item: "Milkshake",
-      price: 70,
-      image: "https://placehold.co/220x135",
+      item: "Iced Mocha",
+      price: 50,
+      image: Iced_Mocha,
     },
     {
       id: 8,
       category: "Drinks",
-      item: "Iced Tea",
+      item: "Mocktail",
+      price: 70,
+      image: Mocktail,
+    },
+    {
+      id: 9,
+      category: "Drinks",
+      item: "Smoothie",
       price: 60,
-      image: "https://placehold.co/220x135",
+      image: Smoothie,
     },
   ];
   
